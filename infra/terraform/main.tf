18346bf4188d8683e0990cd38db6972875853621
Brian Anderson <brian@briananderson.xyz>
2026-01-23 09:03:47 -0700

    test: trigger PR workflow again

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -103,3 +103,4 @@ output "ci_service_account" { value = google_service_account.github_ci.email }
 output "site_bucket" { value = google_storage_bucket.site.url }
 output "site_dev_bucket" { value = google_storage_bucket.site_dev.url }
 # test PR workflow fix
+# trigger PR workflow
